a49f7d0a72094a3690dc2ea0ff8ba009e019d534
DevaWinner <winneraaniekan@gmail.com>
2023-02-16 23:31:33 +0000

    edit to 5 and 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,5 @@
-/*
- * File: 5-printf.c
- * Auth: Benjamin M Musyoki
- */
-
 #include <stdio.h>
 
-/**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");

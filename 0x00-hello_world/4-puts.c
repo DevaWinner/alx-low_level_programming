d27961249cf19d0feaeb7c2297f5f09529f0d712
DevaWinner <winneraaniekan@gmail.com>
2023-02-16 23:33:15 +0000

    edit to 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,5 @@
-/*
- * File: 4-puts.c
- * Auth: Benjamin M Musyoki
- */
-
 #include <stdio.h>
 
-/**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: Always 0.
- */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

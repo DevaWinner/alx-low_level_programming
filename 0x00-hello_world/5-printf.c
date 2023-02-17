d8eb905153ace2bcbaa7dbf5a0978f4c007c709a
DevaWinner <winneraaniekan@gmail.com>
2023-02-16 16:02:49 -0800

    update to 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");

237f79c1f64552ee09e2ef232a000c79a9588b8f
DevaWinner <winneraaniekan@gmail.com>
2023-03-21 18:57:49 -0700

    3

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,4 +1,12 @@
-#ifndef _PI_H_
-#define _PI_H_
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
 #define PI 3.14159265359
-#endif /* _PI_H_ */
+
+#endif
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the file
-*
-* Return: always 0
-*/
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
